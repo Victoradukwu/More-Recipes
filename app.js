da7b715645f40f0d50887979bf274c568fea57e9
Andela Developer <vicads01@gmail.com>
2017-12-21 04:28:08 +0100

    set up react folder structure

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
 import router from './server/routes/index';
+import webpackConfig from './webpack.config';
+
 
 const userRoute = router.user;
 const recipeRoute = router.recipe;
@@ -16,11 +21,16 @@ const app = express();
 // Log requests to the console.
 app.use(logger('dev'));
 
+if (process.env.NODE_ENV !== 'test') {
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, webpackConfig.devServer));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static('template'));
+app.use(express.static('client/src/public'));
 
 app.use(userRoute);
 app.use(recipeRoute);
@@ -36,8 +46,6 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.all('*', (req, res) => res.status(404).send({
-  message: 'Page does not exist',
-}));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './client/src/public/index.html')));
 
 export default app;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import 'bootstrap';
 
 const app = <p>Just testing</p>;
 const appRoot = document.getElementById('app');
 
 
-ReactDOM.render(app,appRoot);
+ReactDOM.render(app, appRoot);
