86638ca38ed01021f7701a8b56e23643dee51227
Victoradukwu <vicads01@gmail.com>
2018-01-21 00:26:20 +0100

    Delete .DS_Store

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const app = <p>Just testing</p>;
 const appRoot = document.getElementById('app');
