5a6ec7791d0568aeb77aab4cfb0aa2e4a4813e5c
Andela Developer <vicads01@gmail.com>
2018-01-21 02:57:15 +0100

    add css and img folders

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style3';
 
 const app = <p>Just testing</p>;
 const appRoot = document.getElementById('app');
