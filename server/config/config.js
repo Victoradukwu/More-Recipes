9de5d1d9d61a787f8bc2776968bec00c9e1c41cc
Andela Developer <vicads01@gmail.com>
2018-02-17 16:20:18 +0100

    fix the test script bug

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,7 @@ module.exports = {
     database: process.env.TEST_DATABASE_NAME,
     host: '127.0.0.1',
     dialect: 'postgres',
+    logging: false
     // use_env_variable: 'TEST_DATABASE_URL',
     // dialect: 'postgres'
   },
